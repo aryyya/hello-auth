e3e39c7ab7bf3b2eafd642d7ccd43466f65ffd58
aryyya <arya@live.ca>
2018-07-08 17:01:21 -0700

    Add body parser.

--- server/main.js
+++ server/main.js
@@ -1,26 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 const server = express()
 const port = process.env.PORT || 8080
 
 server.use(morgan('dev'))
+server.use(bodyParser.json())
 
 server.get('/', (req, res) => {
     res.json({
         message: 'Hello, world!'
     })
-})
-
-server.get('/time', (req, res) => {
-    res.json({
-        time: Date.now()
-    })
-})
-
-server.post('/user', (req, res) => {
-    console.log(req.body.username)
-    console.log(req.body.password)
 })
 
 server.listen(port, () => {
