349c6e70f093d870da5507c86ccc27931bd4e46f
aryyya <arya@live.ca>
2018-07-09 01:15:13 -0700

    Implement top bar in vanilla js/css.

--- client/src/App.js
+++ client/src/App.js
@@ -3,56 +3,31 @@ import './App.css'
 
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
+import TopBar from './components/top-bar'
 import Home from './components/home'
 import SignUp from './components/sign-up'
 import LogIn from './components/log-in'
-
-import { withStyles } from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  flex: {
-    flex: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-}
 
 class App extends Component {
   render() {
     const { classes } = this.props
     return (
-      <Router>
-        <div className={classes.root}>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-                <MenuIcon open={false} />
-              </IconButton>
-              <Typography variant="title" color="inherit" className={classes.flex}>hello-auth</Typography>
-              <Button color="inherit" component={Link} to="/log-in">Log In</Button>
-              <Button color="inherit" component={Link} to="/sign-up">Sign Up</Button>
-            </Toolbar>
-          </AppBar>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/sign-up" component={SignUp} />
-          <Route path="/log-in" component={LogIn} />
-        </Switch>
-        </div>
-      </Router >
+      <div className="app">
+        <Router>
+          <div>
+            <TopBar />
+            <div class="app__page">
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/sign-up" component={SignUp} />
+                <Route path="/log-in" component={LogIn} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </div>
     )
   }
 }
 
-export default withStyles(styles)(App)
+export default App
---
+++ client/src/components/top-bar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import './top-bar.css'
+
+import { Link } from 'react-router-dom'
+
+const pages = [
+  { name: 'Sign up', path: '/sign-up' },
+  { name: 'Log in', path: '/log-in' }
+]
+
+const getLink = page => (
+  <Link className="top-bar__link" to={page.path}>{page.name}</Link>
+)
+
+const getLinkItem = page => (
+  <li className="top-bar__link-item">
+    {getLink(page)}
+  </li>
+)
+
+class TopBar extends Component {
+  render () {
+    return (
+      <div className="top-bar">
+        <div className="top-bar__title">
+          {getLink({name: 'hello-auth', path: '/'})}
+        </div>
+        <ul className="top-bar__links-list">
+          {pages.map(getLinkItem)}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default TopBar
