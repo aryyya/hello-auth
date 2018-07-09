cccc5cabba7fdcddd278648cd312f3cf83f3e592
aryyya <arya@live.ca>
2018-07-08 21:30:53 -0700

    Add Material-UI and top bar.

--- client/src/App.js
+++ client/src/App.js
@@ -7,31 +7,52 @@ import Home from './components/home'
 import SignUp from './components/sign-up'
 import LogIn from './components/log-in'
 
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/core/Menu'
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+}
+
 class App extends Component {
-  render () {
+  render() {
+    const { classes } = this.props
     return (
       <Router>
-        <div className="app">
-          <ul>
-            <li>
-              <Link to="/">Home Page</Link>
-            </li>
-            <li>
-              <Link to="/sign-up">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/log-in">Log In</Link>
-            </li>
-          </ul>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/sign-up" component={SignUp} />
-            <Route path="/log-in" component={LogIn} />
-          </Switch>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                <MenuIcon open={false} />
+              </IconButton>
+              <Typography variant="title" color="inherit" className={classes.flex}>hello-auth</Typography>
+              <Button color="inherit" component={Link} to="/log-in">Log In</Button>
+              <Button color="inherit" component={Link} to="/sign-up">Sign Up</Button>
+            </Toolbar>
+          </AppBar>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/sign-up" component={SignUp} />
+          <Route path="/log-in" component={LogIn} />
+        </Switch>
         </div>
-      </Router>
+      </Router >
     )
   }
 }
 
-export default App
+export default withStyles(styles)(App)
