c87b6abd2a341dd50e6ff14826b29b087944a53e
aryyya <arya@live.ca>
2018-07-09 20:05:04 -0700

    Create JSON Web Token on login.

--- server/main.js
+++ server/main.js
@@ -1,18 +1,49 @@
+// MODULES
+
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+
+// SERVER
 
 const server = express()
 const port = process.env.PORT || 8080
 
+// MIDDLEWARE
+
 server.use(morgan('dev'))
 server.use(bodyParser.json())
 
+// IN MEMORY DATABASE
+
+const getUserId = (() => {
+  let id = 0
+  return () => id++
+})()
+
 const users = [
-  { id: 0, username: 'bill_gates', password: 'microsoft' },
-  { id: 1, username: 'steve_jobs', password: 'apple' },
-  { id: 2, username: 'linus_torvalds', password: 'linux' }
+  { id: getUserId(), username: 'bill_gates', password: 'microsoft', roles: [ 'standard' ] },
+  { id: getUserId(), username: 'steve_jobs', password: 'apple', roles: [ 'standard' ] },
+  { id: getUserId(), username: 'linus_torvalds', password: 'linux', roles: [ 'administrator' ] }
 ]
+
+// JWT
+
+const secret = 'Whatever happened to Gary Cooper? The strong, silent type.'
+
+const getJWT = ({ id, roles }) => {
+  return jwt.sign({
+    id,
+    roles
+  }, secret, { expiresIn: 30 })
+}
+
+const getJWTPayload = token => {
+  return jwt.verify(token, secret)
+}
+
+// ROUTES
 
 server.get('/', (req, res) => {
   res.json({
@@ -29,15 +60,29 @@ server.get('/secret', (req, res) => {
 server.post('/login', (req, res) => {
   const { username, password } = req.body
 
-  let isValid = false
-  users.forEach(user => {
-    if (user.username === username && user.password === password) {
-      isValid = true
+  let user = null
+  users.forEach(dbUser => {
+    if (dbUser.username === username && dbUser.password === password) {
+      user = dbUser
     }
   })
 
-  res.json({ status: isValid ? 'success' : 'failure' })
+  if (!user) {
+    res.json({ status: 'failure' })
+  }
+
+  const token = getJWT({
+    id: user.id,
+    roles: user.roles
+  })
+
+  res.json({
+    status: 'success',
+    token
+  })
 })
+
+// START SERVER
 
 server.listen(port, () => {
   console.log(`The server is listening on port ${port}.`)
